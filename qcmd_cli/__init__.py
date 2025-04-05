e051fa9c3ee98358b3445c04f86c97e092749872
ibrahim <al-edanee@hotmail.com>
2025-04-05 15:08:54 +0000

    Bump version to 1.0.13 to fix PyPI upload issue

--- qcmd_cli/__init__.py
+++ qcmd_cli/__init__.py
@@ -2,7 +2,7 @@
 QCMD - Iraqi-powered Command Generation Tool.
 """
 
-__version__ = "1.0.12"
+__version__ = "1.0.13"
 
 # Export post_install module for the post-installation script
 from . import qcmd, post_install 
