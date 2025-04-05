76036ec6716b5e02934ff65bee4ca20bdd96771e
ibrahim <al-edanee@hotmail.com>
2025-04-05 15:36:10 +0000

    Bump version to 1.0.14

--- qcmd_cli/__init__.py
+++ qcmd_cli/__init__.py
@@ -2,7 +2,7 @@
 QCMD - Iraqi-powered Command Generation Tool.
 """
 
-__version__ = "1.0.13"
+__version__ = "1.0.14"
 
 # Export post_install module for the post-installation script
 from . import qcmd, post_install 
